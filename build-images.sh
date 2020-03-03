0dbff9536bbb6a7a042e50e5af5f5793fb37c2e3
FT <faisal.ajmal@gmail.com>
2020-03-03 20:33:16 +0300

    bug fix

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t faisalthaheem/simanno:ws-2.0 -f ./docker/ws/Dockerfile ./code/
-docker build -t faisalthaheem/simanno:web-2.0 -f ./docker/web/Dockerfile ./code/
+docker build -t faisalthaheem/simanno:ws-2.0 -f ./docker/ws/Dockerfile .
+docker build -t faisalthaheem/simanno:web-2.0 -f ./docker/web/Dockerfile .
